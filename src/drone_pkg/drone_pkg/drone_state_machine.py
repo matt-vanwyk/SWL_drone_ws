0c1cccacc74f1bf0fd354bdff466d8b7c7e1438b
matt-vanwyk <matt.vanwyk2@gmail.com>
2025-09-12 17:18:44 +0200

    Added DroneCommand.srv to shred_interfaces.

---
+++ src/drone_pkg/drone_pkg/drone_state_machine.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rclpy
+import time
+import threading
+from swl_shared_interfaces.srv import DroneCommand
+from rclpy.node import Node
+from statemachine import StateMachine, State
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.task import Future
+
+class DroneStateMachine(StateMachine):
+
+    # Define States
+    Idle = State(initial=True)
+    Ready_To_Fly = State()
+
+    # Define Transitions
+    initialise_drone = Idle.to(Ready_To_Fly)
+
+    def on_enter_Idle(self):
+        """Called when entering Idle state"""
+        self.model.get_logger().info("System boot... collecting telem")
+        
+    def on_enter_Ready_To_Fly(self):
+        """Called when entering Home state"""
+        self.model.get_logger().info("Entered READY_TO_FLY state - drone ready to receive missions")
+
+class DroneStateMachineNode(Node):
+    def __init__(self):
+        super().__init__('drone_state_machine')
+        self.state_machine = DroneStateMachine(model=self)
+
+        self.callback_group = ReentrantCallbackGroup()
+
--- src/drone_pkg/setup.py
+++ src/drone_pkg/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'drone_state_machine = drone_package.drone_state_machine:main'
         ],
     },
 )
